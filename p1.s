09d0d12bd85a558880f548732537cf4bcdc93107
DarrylMcCottrell <43862357+DarrylMcCottrell@users.noreply.github.com>
2020-02-19 15:40:43 -0600

    Add files via upload

--- p1.s
+++ p1.s
@@ -19,36 +19,37 @@
                 # Dictionary:
                ldr                 x20, =i  // Registered x20 as i        
                bl                  printf // Making capable to print off
-               mov                 x2, 100 // registered x2 as 100 to used for my for-loop
-               bl                  printf
+             //  mov                 x2, 100 // registered x2 as 100 to used for my for-loop
+             //  bl                  printf
                ldr                 x21, =pflag // registered x21 as pflag
                bl                  printf
-               ldr                 x3, 3 // registered x3 as 3 so I can use in udiv     
+             //  ldr                 x3, 3 // registered x3 as 3 so I can use in udiv     
                bl                  printf
-               ldr                 x4, 5 // registered x4 as 5 for udiv
-
+               //ldr                 x4, 5 // registered x4 as 5 for udiv
+               bl                  printf
                 # restore registers
+
+                mov                x20, 0  // initializing i as 0
                  
  top:
                                  
   
-                mov                x20, 0  // initializing i as 0
-                cmp                x2, #0  // starting for loop comparing 100 to 0
-                bgt                .endloop  // branch is greater than 100 in the for loop
-                b                  top
+               
+                cmp                x20, 100  // starting for loop comparing 100 to 0
+                bge                endloop  // branch is greater than 100 in the for loop
                 ldr                x21, =pflag  // loading in pflag
                 mov                x21, 0    // intializing pflag as 0
             
                 cmp                 x20, 3  // if statement to compare i % 3
-               //   udiv                x3, x2, x3 // Would I use udiv to find if the number is divisible by 5?
-            //    msub
+               // udiv               x20, x20, x3 // Would I use udiv to find if the number is divisible by 5?
+               // msub
                 bne                 bottom // if it's not equal send to the bottom
                 ldr                 x0,=fizz // if its divisble by 3, it will load fizz
                 bl                  printf  // print off fizz
                 mov                 x21, 1  // intializing the pflag as 1 in the if statement
                 cmp                 x20, 5  // if statement to compare i to 5
              // udiv                x4, x2, x4
-             //   msub
+             // msub
                 bne                 bottom 
                 ldr                 x0, =buzz
                 bl                  printf
@@ -57,7 +58,9 @@
                 ldr                 x0, =fmt
                 bl                  printf
                 ldr                 x0, =nl
-                bl                  printf               
+                bl                  printf   
+                add                 x20, x20, 1  //Incrementing it up 1 so it's not an infinite loop
+                b                   top            
                 
                 
                 .endloop:
@@ -82,6 +85,7 @@ fmt:           .asciz        "%d"
 i:             .asciz       "i"
 pflag:         .asciz       "pflag"
 fizzbuzz:      .asciz       "FizzBuzz"
+endloop:       .asciz       "endloop"
 
                     .end
                     
